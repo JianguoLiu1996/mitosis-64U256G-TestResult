fc97f8c32e101f79aa0089253db8476771932fca
JianguoLiu1996 <491925717@qq.com>
2023-07-14 09:14:11 +0800

    mysql test result on 64U256G, innodb_buffer_pool_size=200G

---
+++ mysqlTestResult/mysqlTestBysysbench_innodb_buffer_pool_size200GB_128_10M_F_OFF/mysqlTestBysysbench_innodb_buffer_pool_size200GB_128_10M_F_OFF.sh
@@ -0,0 +1,344 @@
+#!/bin/bash
+OUTPUTPATH=./
+NUMBER=1nd
+CONFIG=F_OFF_128_10M
+function preparedata(){
+	echo "==Prepare data=="
+	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+                --threads=100 \
+                --oltp-table-size=10000000 \
+                --oltp-tables-count=128 \
+                --mysql-db=testdb \
+                --db-driver=mysql \
+                --mysql-host=localhost \
+                --mysql-port=3306 \
+                --mysql-user=root \
+                --mysql-password=123456 \
+                --report-interval=3 prepare >> "${OUTPUTPATH}mysysbench_prepare_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+        wait
+        sleep 1m
+}
+
+function cleardata(){
+	echo "==cleanup sysbench testdb of RW=="
+	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+               --threads=200 \
+               --time=1800 \
+               --max-requests=0 \
+               --mysql-db=testdb \
+               --oltp-table-size=10000000 \
+               --oltp-tables-count=128 \
+               --db-driver=mysql \
+               --mysql-host=localhost \
+               --mysql-port=3306 \
+               --mysql-user=root \
+               --mysql-password=123456 \
+               --report-interval=3 \
+               --forced-shutdown=1 cleanup
+       wait
+       sleep 1m
+}
+
+function preheatingData(){
+	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+		--db-driver=mysql \
+		--mysql-host=localhost \
+		--mysql-user=root \
+		--mysql-password=123456 \
+		--mysql-port=3306 \
+		--mysql-db=testdb \
+		--threads=200 \
+		--events=0 \
+		--time=600 \
+		--oltp-table-size=10000000 \
+		--oltp-tables-count=64 \
+		--oltp-test-mode=complex \
+		--oltp-read-only=on \
+		--max-requests=0 \
+		--forced-shutdown=1 run
+}
+
+function rwtest(){
+#	echo "==Prepare data for RW=="
+#	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#		--threads=100 \
+#		--oltp-table-size=10000000 \
+#		--oltp-tables-count=64 \
+#		--mysql-db=testdb \
+#		--db-driver=mysql \
+#		--mysql-host=localhost \
+#		--mysql-port=3306 \
+#		--mysql-user=root \
+#		--mysql-password=123456 \
+#		--report-interval=3 prepare >> "${OUTPUTPATH}mysysbench_rw_prepare_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+#	wait
+#	sleep 1m
+
+	echo "==run sysbench for RW=="
+	echo "Start time is: $(date '+%c')"
+	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+		--threads=200 \
+		--time=1800 \
+		--max-requests=0 \
+		--mysql-db=testdb \
+		--oltp-table-size=10000000 \
+		--oltp-tables-count=128 \
+		--db-driver=mysql \
+		--mysql-host=localhost \
+		--mysql-port=3306 \
+		--mysql-user=root \
+		--mysql-password=123456 \
+		--report-interval=3 \
+		--forced-shutdown=1 run >> "${OUTPUTPATH}mysysbench_rw_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+	echo "End time is: $(date '+%c')"
+	wait
+	sleep 1m
+
+#	echo "==cleanup sysbench testdb of RW=="
+#	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#	 	--threads=200 \
+#	 	--time=1800 \
+#	 	--max-requests=0 \
+#	 	--mysql-db=testdb \
+#	 	--oltp-table-size=10000000 \
+#		--oltp-tables-count=64 \
+#	 	--db-driver=mysql \
+#		--mysql-host=localhost \
+#		--mysql-port=3306 \
+#		--mysql-user=root \
+#	 	--mysql-password=123456 \
+#	 	--report-interval=3 \
+#	 	--forced-shutdown=1 cleanup
+#	wait
+#	sleep 1m
+}
+
+function rotest(){
+#	echo "==Prepare data for RO=="
+#	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#	 	--threads=100 \
+#	 	--oltp-table-size=10000000 \
+#	 	--oltp-tables-count=64 \
+#	 	--mysql-db=testdb \
+#	 	--db-driver=mysql \
+#	 	--mysql-host=localhost \
+#	 	--mysql-port=3306 \
+#	 	--mysql-user=root \
+#	 	--mysql-password=123456 \
+#	 	--report-interval=3 prepare >> "${OUTPUTPATH}mysysbench_ro_prepare_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+#
+#	wait
+#	sleep 1m
+
+	echo "==run sysbench test for RO=="
+	echo "Start time is: $(date '+%c')"
+	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+		--mysql-host=localhost \
+		--oltp-tables-count=128 \
+		--mysql-user=root \
+		--mysql-password=123456 \
+		--mysql-port=3306 \
+		--db-driver=mysql \
+		--oltp-table-size=10000000 \
+		--mysql-db=testdb \
+		--max-requests=0 \
+		--oltp-simple-ranges=0 \
+		--oltp-distinct-ranges=0 \
+		--oltp-sum-ranges=0 \
+		--oltp-order-ranges=0 \
+		--time=1800 \
+		--oltp-read-only=on \
+		--threads=200 \
+		--report-interval=3 \
+		--thread-init-timeout=300 \
+		--forced-shutdown=1 run >> "${OUTPUTPATH}mysysbench_ro_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+	echo "End time is: $(date '+%c')"
+	wait
+	sleep 1m
+
+#	echo "==cleanup sysbench testdb for RO=="
+#	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#		--mysql-host=localhost \
+#		--oltp-tables-count=64 \
+#		--mysql-user=root \
+#		--mysql-password=123456 \
+#		--mysql-port=3306 \
+#		--db-driver=mysql \
+#		--oltp-table-size=10000000 \
+#		--mysql-db=testdb \
+#		--max-requests=0 \
+#		--oltp-simple-ranges=0 \
+#		--oltp-distinct-ranges=0 \
+#		--oltp-sum-ranges=0 \
+#		--oltp-order-ranges=0 \
+#		--time=1800 \
+#		--oltp-read-only=on \
+#		--threads=200 \
+#		--report-interval=3 \
+#		--thread-init-timeout=300 \
+#		--forced-shutdown=1 cleanup
+#	wait
+#	sleep 1m
+}
+
+function wotest(){
+#	echo "==Prepare data for WO=="
+#	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#	 	--threads=100 \
+#	 	--oltp-table-size=10000000 \
+#	 	--oltp-tables-count=64 \
+#	 	--mysql-db=testdb \
+#	 	--db-driver=mysql \
+#	 	--mysql-host=localhost \
+#	 	--mysql-port=3306 \
+#	 	--mysql-user=root \
+#	 	--mysql-password=123456 \
+#	 	--report-interval=3 prepare >> "${OUTPUTPATH}mysysbench_wo_prepare_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+#	wait
+#	sleep 1m
+
+	echo "== run sysbench for WO=="
+	echo "Start time is: $(date '+%c')"
+	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+		--mysql-host=localhost \
+		--oltp-tables-count=128 \
+		--mysql-user=root \
+		--mysql-password=123456 \
+		--mysql-port=3306 \
+		--db-driver=mysql \
+		--oltp-table-size=10000000 \
+		--mysql-db=testdb \
+		--max-requests=0 \
+		--max-time=1800 \
+		--oltp-simple-ranges=0 \
+		--oltp-distinct-ranges=0 \
+		--oltp-sum-ranges=0 \
+		--oltp-order-ranges=0 \
+		--oltp-point-selects=0 \
+		--threads=200 \
+		--randtype=uniform \
+		--report-interval=3 \
+		--thread-init-timeout=300 \
+		--forced-shutdown=1 run >> "${OUTPUTPATH}mysysbench_wo_${CONFIG}_${NUMBER}_$(date +"%Y%m%d%H%M%S").log"
+	echo "End time is: $(date '+%c')"
+	wait
+	sleep 1m
+
+#	echo "== run sysbench for WO"
+#       	sysbench /home/jianguoliu/Download/sysbench-1.0.11/tests/include/oltp_legacy/oltp.lua \
+#		--mysql-host=localhost \
+#                --oltp-tables-count=64 \
+#                --mysql-user=root \
+#                --mysql-password=123456 \
+#                --mysql-port=3306 \
+#                --db-driver=mysql \
+#                --oltp-table-size=10000000 \
+#                --mysql-db=testdb \
+#                --max-requests=0 \
+#                --max-time=1800 \
+#                --oltp-simple-ranges=0 \
+#                --oltp-distinct-ranges=0 \
+#                --oltp-sum-ranges=0 \
+#                --oltp-order-ranges=0 \
+#                --oltp-point-selects=0 \
+#                --threads=200 \
+#                --randtype=uniform \
+#                --report-interval=3 \
+#                --thread-init-timeout=300 \
+#                --forced-shutdown=1 cleanup
+#	wait
+#	sleep 1m
+#	echo "test end"
+}
+
+function startMysql(){
+	#sudo /etc/init.d/mysql restart
+	echo "===begin to stop mysql==="
+	sudo service mysql stop
+	sleep 5s
+	echo "===success start mysql==="
+
+	echo "===disable Auto-numa==="
+	sudo echo 0 > /proc/sys/kernel/numa_balancing
+	sleep 5s
+	echo "===success disable Auto-numa==="
+
+	echo "cat /proc/sys/kernel/numa_balancing"
+	cat /proc/sys/kernel/numa_balancing
+	
+	echo "===begin to start mysql==="
+	sudo service mysql start
+	echo "success start mysql==="
+	sleep 30s
+}
+
+function startMysqlWithPageReplication(){
+        #sudo /etc/init.d/mysql restart
+        echo "===begin to stop mysql==="
+        sudo service mysql stop
+        sleep 5s
+        echo "===success start mysql==="
+
+        echo "===disable Auto-numa==="
+        sudo echo 0 > /proc/sys/kernel/numa_balancing
+        sleep 5s
+        echo "===success disable Auto-numa==="
+
+        echo "cat /proc/sys/kernel/numa_balancing"
+        cat /proc/sys/kernel/numa_balancing
+
+        echo "===begin to start mysql==="
+        sudo numactl -r 0-3 service mysql start
+        echo "success start mysql==="
+        sleep 30s
+}
+
+#three times test
+function alltest(){
+	start_time=$(date +%s)  # script start run time
+	
+	startMysql
+	#startMysqlWithPageReplication
+	preparedata
+	
+	#rw test
+	OUTPUTPATH=./
+	NUMBER=1nd
+	rwtest
+	OUTPUTPATH=../mysqlTestBysysbench_innodb_buffer_pool_size200GB_128_10M_F_OFF_2nd/
+	NUMBER=2nd
+	rwtest
+	OUTPUTPATH=../mysqlTestBysysbench_innodb_buffer_pool_size200GB_128_10M_F_OFF_3nd/
+        NUMBER=3nd
+	rwtest
+
+	#ro test
+	OUTPUTPATH=./
+        NUMBER=1nd
+	rotest
+	OUTPUTPATH=../mysqlTestBysysbench_innodb_buffer_pool_size200GB_128_10M_F_OFF_2nd/
+	NUMBER=2nd
+	rotest
+	OUTPUTPATH=../mysqlTestBysysbench_innodb_buffer_pool_size200GB_128_10M_F_OFF_3nd/
+        NUMBER=3nd
+	rotest
+	
+	#wo test
+	OUTPUTPATH=./
+        NUMBER=1nd
+	wotest
+	OUTPUTPATH=../mysqlTestBysysbench_innodb_buffer_pool_size200GB_128_10M_F_OFF_2nd/
+	NUMBER=2nd
+	wotest
+	OUTPUTPATH=../mysqlTestBysysbench_innodb_buffer_pool_size200GB_128_10M_F_OFF_3nd/
+        NUMBER=3nd
+	wotest
+
+	#cleardata
+
+	end_time=$(date +%s)  # scrip stop run time,(s).
+	# calculate script run time,(s).
+	duration=$((end_time - start_time))
+	echo "Script run time is: $duration (s)"
+}
+alltest
